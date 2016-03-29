b6fc48b321256c098830fc771d90350d39ccaee0
Teruaki Yokoyama <tel1203@gmail.com>
2016-03-29 19:40:47 +0900

    add usage, STDOUT.sync

--- pm25.rb
+++ pm25.rb
@@ -1,8 +1,13 @@
 #
 # pm25.rb : getting measurement data about PM2.5 from Sinnei sensor
 #
+# Usage:
+#   $ ruby pm25.rb 192.168.101.4 10001
+#
+
 
 require "./lib_sinnei_pm25.rb"
+STDOUT.sync = true
 
 # READ DATA
 # measurement time (sec)
